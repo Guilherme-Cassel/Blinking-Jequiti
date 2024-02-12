58c84a596c60816eab2abaa4c945525f562d217c
Guilherme Cassel <guilherme.cassel2004@gmail.com>
2024-02-12 12:50:03 -0300

    added early maded classes

---
+++ BlinkingJequiti/JequitiPipeServer.cs
@@ -0,0 +1,45 @@
+﻿using System.IO.Pipes;
+
+/// <summary>
+/// Pipe Name = "BlinkingJequiti"
+/// </summary>
+public class JequitiPipeServer : IDisposable
+{
+    private const string PipeName = "BlinkingJequiti";
+    private readonly NamedPipeServerStream pipeServer = new(PipeName, PipeDirection.InOut);
+
+    internal async Task<string?> StartReading()
+    {
+        string? result = string.Empty;
+
+        try
+        {
+            await pipeServer.WaitForConnectionAsync();
+
+            using StreamReader reader = new (pipeServer);
+
+            result = reader.ReadLine();
+
+            reader.Close();
+        }
+        catch (Exception ex)
+        {
+            MessageBox.Show("Erro: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+        }
+
+        if (result is not null)
+            result = result.ToLower().Trim();
+
+        return result??string.Empty;
+    }
+
+    public void Dispose()
+    {
+        pipeServer.Close();
+        pipeServer.Dispose();
+        GC.Collect();
+        GC.WaitForPendingFinalizers();
+        GC.SuppressFinalize(this);
+    }
+}
+
---
+++ BlinkingJequiti/UserState.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Console
+{
+    public partial class UserState
+    {
+        public static bool IsConnected => UserInactiveTime() < 5;
+        private static int UserInactiveTime()
+        {
+            uint idleTicks = 0;
+            LASTINPUTINFO lastInputInfo = new();
+            lastInputInfo.cbSize = (uint)Marshal.SizeOf(lastInputInfo);
+            lastInputInfo.dwTime = 0;
+
+            if (GetLastInputInfo(ref lastInputInfo))
+            {
+                idleTicks = (uint)Environment.TickCount - lastInputInfo.dwTime;
+            }
+
+            uint idleSeconds = idleTicks / 1000;
+
+            return (int)idleSeconds;
+        }
+
+        [LibraryImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static partial bool GetLastInputInfo(ref LASTINPUTINFO plii);
+        private struct LASTINPUTINFO
+        {
+            public uint cbSize;
+            public uint dwTime;
+        }
+    }
+}
