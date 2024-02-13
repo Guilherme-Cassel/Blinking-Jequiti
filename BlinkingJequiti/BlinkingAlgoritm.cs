6d6ddab5eda5e292f4ebf63559e9c1cb476b3480
Guilherme Cassel <150675837+Guilherme-Cassel@users.noreply.github.com>
2024-02-13 20:57:59 -0300

    Added BlinkingAlgotirm class

---
+++ BlinkingJequiti/BlinkingAlgoritm.cs
@@ -0,0 +1,49 @@
+﻿namespace KernelAgent
+{
+    public class BlinkingAlgoritm
+    {
+        const int OneHourInMiliseconds = 3600000;
+        const int TreeHoursInMilisecons = 10800000;
+
+        static void LoopThroughJequitiBlink()
+        {
+
+            while (true)
+            {
+                Thread.Sleep(new Random()
+                    .Next(OneHourInMiliseconds, TreeHoursInMilisecons));
+
+                BlinkForm1();
+            }
+        }
+
+        static async void BlinkForm1()
+        {
+            try
+            {
+                using Form1 form1 = new();
+                form1.Hide();
+
+                await Task.Delay(1000);
+
+                if (form1.InvokeRequired)
+                {
+                    form1.Invoke(new Action(BlinkForm1));
+                    return;
+                }
+
+                form1.Visible = true;
+                await Task.Delay(100);
+                form1.Visible = false;
+
+                form1.Close();
+                form1.Dispose();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+    }
+}
